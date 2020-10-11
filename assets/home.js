b381d48ba5380c1e987cbd6e89f555e66a1431e2
gregth-dev <gregory.thorel@live.fr>
2020-10-11 03:06:37 +0200

    vue install et rendu sur home

--- assets/home.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import home from './pages/home';
-
-const app = new Vue({
-    render: (h) => h(home),
-
-}).$mount('#app');
--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
-    .addEntry('home', './assets/home.js')
+    .addEntry('app', './assets/js/app.js')
+    //.addEntry('home', './assets/home.js')
     //.addEntry('page2', './assets/page2.js')
 
     .enableVueLoader()
